c6d0acd60ce4047b0105e8d6e4871215e3fa14f0
Victor Almeida <victooralmeida1@gmail.com>
2021-04-10 15:00:14 -0300

    feature: Implementa tela de perfil

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,10 +1,62 @@
 import React from 'react';
-import { Container, Text } from './styles';
+import {
+  Container,
+  Header,
+  ProfilePicture,
+  ProfileName,
+  WrapperButton,
+  Content,
+  MenuText,
+  MenuIcon,
+  ChevronIcon,
+  LeftContent,
+} from './styles';
 
 const Profile = () => {
   return (
     <Container>
-      <Text>Profile</Text>
+      <Header>
+        <ProfilePicture
+          source={{
+            uri:
+              'https://scontent.fcgh23-1.fna.fbcdn.net/v/t1.6435-9/89913643_2331855266915171_3122078277980651520_n.jpg?_nc_cat=103&ccb=1-3&_nc_sid=09cbfe&_nc_eui2=AeHU9XZ0_-1ZsUl8mY93BEFNulWZ6i83oFW6VZnqLzegVUFg0EL1oZGVKXt8GjxyA0qeYUYg7hOt-t1Uyc46Vawg&_nc_ohc=6brkL3cpCIsAX8MHL2Q&_nc_ht=scontent.fcgh23-1.fna&oh=a4ec1749ea5585475862652219611a43&oe=6095973B',
+          }}
+        />
+        <ProfileName>Victor Almeida</ProfileName>
+      </Header>
+      <Content>
+        <WrapperButton>
+          <LeftContent>
+            <MenuIcon name="user" />
+            <MenuText>minha conta</MenuText>
+          </LeftContent>
+
+          <ChevronIcon name="chevron-right" />
+        </WrapperButton>
+        <WrapperButton>
+          <LeftContent>
+            <MenuIcon name="credit-card" />
+            <MenuText>forma de pagamento</MenuText>
+          </LeftContent>
+          <ChevronIcon name="chevron-right" />
+        </WrapperButton>
+        <WrapperButton>
+          <LeftContent>
+            <MenuIcon name="user-plus" />
+            <MenuText>indicar amigos</MenuText>
+          </LeftContent>
+
+          <ChevronIcon name="chevron-right" />
+        </WrapperButton>
+        <WrapperButton>
+          <LeftContent>
+            <MenuIcon name="question" />
+            <MenuText>ajuda</MenuText>
+          </LeftContent>
+
+          <ChevronIcon name="chevron-right" />
+        </WrapperButton>
+      </Content>
     </Container>
   );
 };
--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -1,1 +1,59 @@
-export { Container, Header, Text, Logo, styles } from '../styles';
+import styled from 'styled-components/native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+
+export { Container, Text, Logo, styles } from '../styles';
+
+export const Content = styled.View`
+  flex: 1;
+  padding: 20px;
+`;
+export const Header = styled.View`
+  flex: 0.4;
+  background-color: #ddd;
+  align-items: center;
+  flex-direction: row;
+  padding: 30px;
+`;
+
+export const ProfilePicture = styled.Image`
+  width: 80px;
+  height: 80px;
+  border-radius: 40px;
+  margin: 5px;
+`;
+
+export const ProfileName = styled.Text`
+  font-size: 30px;
+  font-family: Nunito-ExtraBold;
+  margin: 10px 20px;
+`;
+
+export const WrapperButton = styled.Pressable`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin: 20px 0;
+  elevation: 2;
+`;
+
+export const LeftContent = styled.View`
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  flex: 1;
+`;
+
+export const MenuText = styled.Text`
+  font-size: 22px;
+  font-family: Nunito-Regular;
+`;
+export const MenuIcon = styled(Icon)`
+  font-size: 22px;
+  color: #ccc;
+  width: 40px;
+`;
+
+export const ChevronIcon = styled(Icon)`
+  font-size: 22px;
+  color: #ccc;
+`;
